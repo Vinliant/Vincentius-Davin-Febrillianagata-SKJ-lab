66b55167f99a832a1f05ef17ae0c33f89dfdc3fb
Vinliant <167068938+Vinliant@users.noreply.github.com>
2024-08-30 13:58:07 +0700

    Add files via upload

---
+++ perkalian.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    short num1 = 5;
+    short num2 = 10;
+    short result = 0;
+
+    result = num1 * num2;
+    
+    cout << "Result: " << result << endl;
+    
+    return 0;
+}
